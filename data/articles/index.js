a838a8404a2d70df0ab6d0322b4e811c8e55ecd4
Zied-MF <zieed.fekih@gmaiL.com>
2026-04-12 13:53:13 +0200

    Update index.js

--- data/articles/index.js
+++ data/articles/index.js
@@ -3,6 +3,6 @@ import exercice from './exercice-selon-race'
 import nutrition from './nutrition-chien-mythes'
 import age from './age-chien-sante'
 import regle333 from './regle-333-adoption-chien'
-import monChienNeMangePlusCausesEtQuandSinquieter from '.chien-ne-mange-plus-causes-quand-s-inquieter'
+import monChienNeMangePlusCausesEtQuandSinquieter from './chien-ne-mange-plus-causes-quand-s-inquieter'
 
 export const articles = [surpoids, exercice, nutrition, age, regle333, monChienNeMangePlusCausesEtQuandSinquieter, articleSansTitre]
